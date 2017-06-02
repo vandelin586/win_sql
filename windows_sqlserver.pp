aad3c01fdc489a4d1963607a5eace0c271929fa4
Ryan M Vandelinder <ryan@autostructure.io>
2017-06-01 21:23:07 -0500

    Correct Puppet line spacing

--- windows_sqlserver.pp
+++ windows_sqlserver.pp
@@ -17,21 +17,23 @@ Optional[String] $sec_mode,
   String $dir_tmp,
   String $postinstall,
   String $sql_ver,
-  ){ 
+  ){
 
 # Ensure .NET3.5 is installled
   windowsfeature { 'NET-Framework-Core':
     ensure => present,
     before => Exec['sqlserver_dnld'],
   }
+
   $srvname  = $::facts['hostname']
   $download = "profile/windows/${sql_ver}.ps1"
+
     exec { 'sqlserver_dnld':
       command  => file($download),
       provider => powershell,
       timeout  => 7200,
       creates  => "C:\\Program Files (x86)\\Microsoft SQL Server\\110\\COM\\instapi110.dll",
-    } 
+    }
 
     if ($app_adm_acct != undef) {
       user { $app_adm_acct:
@@ -40,44 +42,41 @@ Optional[String] $sec_mode,
         groups   => ['Administrators'],
       }
 
-    } 
-
+    }
     # Install SQL Server
 
-      sqlserver_instance { $instance:
-        source                => $source,
-        features              => $features,
-        security_mode         => $sec_mode,
-        sql_sysadmin_accounts => $sa_acct,
-        sql_svc_account       => $svc_acct,
-        sql_svc_password      => $svc_pwd,
-        install_switches      => {
-          'TCPENABLED'          => $enable_tcp,
-          'SQLTEMPDBLOGDIR'     => $dir_log,
-          'SQLUSERDBLOGDIR'     => $dir_log,
-          'SQLBACKUPDIR'        => $dir_backup,
-          'SQLTEMPDBDIR'        => $dir_tmp,
-          'INSTALLSQLDATADIR'   => $dir_data,
-          'INSTANCEDIR'         => $dir_inst,
-          'INSTALLSHAREDDIR'    => $dir_share,
-          'INSTALLSHAREDWOWDIR' => $dir_wow,
-          'UpdateEnabled'       => 0,
-        },
+    sqlserver_instance { $instance:
+      source                => $source,
+      features              => $features,
+      security_mode         => $sec_mode,
+      sql_sysadmin_accounts => $sa_acct,
+      sql_svc_account       => $svc_acct,
+      sql_svc_password      => $svc_pwd,
+      install_switches      => {
+        'TCPENABLED'          => $enable_tcp,
+        'SQLTEMPDBLOGDIR'     => $dir_log,
+        'SQLUSERDBLOGDIR'     => $dir_log,
+        'SQLBACKUPDIR'        => $dir_backup,
+        'SQLTEMPDBDIR'        => $dir_tmp,
+        'INSTALLSQLDATADIR'   => $dir_data,
+        'INSTANCEDIR'         => $dir_inst,
+        'INSTALLSHAREDDIR'    => $dir_share,
+        'INSTALLSHAREDWOWDIR' => $dir_wow,
+        'UpdateEnabled'       => 0,
+      },
         require               => User[$app_adm_acct],
-      }
- 
+    }
 
-      sqlserver_features { 'Generic Features':
-        source           => $source,
-        features         => ['IS', 'MDS', 'SSMS'],
-        install_switches => {
-          'UpdateEnabled'  => 0,
-        },
-      } 
 
-      # OPTIONAL Application Admin Group should be members of the Administrators group if provided.
+    sqlserver_features { 'Generic Features':
+      source           => $source,
+      features         => ['IS', 'MDS', 'SSMS'],
+      install_switches => {
+        'UpdateEnabled'  => 0,
+      },
+    }
 
- 
+    # OPTIONAL Application Admin Group should be members of the Administrators group if provided.
 
       # Profile for SQL Post installation
     #  exec { 'sql_post_install':
@@ -85,14 +84,13 @@ Optional[String] $sec_mode,
     #  provider  => powershell,
     #  logoutput => true,
     #  }
- 
 
       # Remove the setup directory & files
-      file {'remove_directory':
-        ensure  => absent,
-        path    => 'C:/DBA',
-        recurse => true,
-        purge   => true,
-        force   => true,
-      }
+    file {'remove_directory':
+      ensure  => absent,
+      path    => 'C:/DBA',
+      recurse => true,
+      purge   => true,
+      force   => true,
+    }
 }
